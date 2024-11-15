4a5d89ba933e8205fe506460ff40077901bc2954
Supriya <supriya@volopay.co>
2024-11-15 17:13:38 +0530

    update files

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Table from './my-table'
-
-const tableName = 'Company Table';
-const headers = ['Company', 'Contact', 'Country'];
-const data = [
-  { Company: 'Alfreds Futterkiste', Contact: 'Maria Anders', Country: 'Germany' },
-  { Company: 'Centro comercial Moctezuma', Contact: 'Francisco Chang', Country: 'Mexico' },
-  { Company: 'Ernst Handel', Contact: 'Roland Mendel', Country: 'Austria' },
-  { Company: 'Island Trading', Contact: 'Helen Bennett', Country: 'UK' },
-  { Company: 'Laughing Bacchus Winecellars', Contact: 'Yoshi Tannamuri', Country: 'Canada' },
-  { Company: 'Magazzini Alimentari Riuniti', Contact: 'Giovanni Rovelli', Country: 'Italy' },
-  { Company: 'Magazzini Alimentari Riuniti', Contact: 'Giovanni Rovelli', Country: 'Italy' },
-];
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Table headers={headers} data={data} tableName={tableName} />
+    <App/>
   </StrictMode>,
 )
