b72ae63e907de5dfefe503d2d8ea8144863a82d7
Supriya <supriya@volopay.co>
2024-11-22 11:42:45 +0530

    added error handling

--- src/Components/Company.jsx
+++ src/Components/Company.jsx
@@ -37,56 +37,46 @@ const Company = () => {
   const xAxisData = annualReports.map((data) => data?.fiscalDateEnding) || [];
   const yAxisData = annualReports.map((data) => data?.totalRevenue) || [];
 
-  // // Handle loading
-  // if (companyOverviewLoading && incomeStatementLoading) {
-  //     return <div>Loading...</div>;
-  // }
-
-  // // Handle errors
-  // if (companyOverviewError && incomeStatementError) {
-  //     return <div>Error loading data. Please try again later.</div>;
-  // }
-
-  // const descriptionPreview =
-  //     companyOverviewData?.Description
-  //         ? companyOverviewData.Description.substring(0, 30)
-  //         : "No description available";
-
-  const descriptionPreview = companyOverviewData?.Description
-    ? companyOverviewData.Description.substring(0, 30)
-    : "No description available";
+  // Handle loading
 
   return (
     <>
-        <div className="flex justify-start items-center bg-slate-200">
-          <div className="w-1/6 border-0 rounded-sm p-8">
-            <img className="w-full overflow-hidden" src={logo} alt="logo" />
+      <div className="flex justify-start items-center bg-slate-200">
+        <div className="w-1/6 border-0 rounded-sm p-8">
+          <img className="w-full overflow-hidden" src={logo} alt="logo" />
+        </div>
+        <div className="w-5/6 flex justify-between items-center">
+          <div>
+            <span className="flex justify-start items-center">
+              <i className="fa-solid fa-building text-neutral-500"></i>
+              <h3 className="m-2 text-base text-neutral-500">
+                {companyOverviewData?.AssetType}
+              </h3>
+            </span>
+            <h2 className="text-2xl">{companyOverviewData?.Name}</h2>
           </div>
-          <div className="w-5/6 flex justify-between items-center">
-            <div>
-              <span className="flex justify-start items-center">
-                <i className="fa-solid fa-building text-neutral-500"></i>
-                <h3 className="m-2 text-base text-neutral-500">{companyOverviewData?.AssetType}</h3>
-              </span>
-              <h2 className="text-2xl">{companyOverviewData?.Name}</h2>
-            </div>
-            <div className="m-8">
-              <button className="me-1 border-2 p-2 border-orange-500 bg-white rounded-lg">
-                <i className="fa-solid fa-bolt m-1 text-orange-500"></i>
-                CONNECT TO CRM
-              </button>
-              <button className="me-2 border-2 p-2 text-white bg-blue-700 rounded-lg">
-                <i className="fa-solid fa-circle-plus m-1"></i>
-                SAVE
-              </button>
-              <button className="mx-2">
-                <i className="fa-solid fa-ellipsis-vertical text-3xl"></i>
-              </button>
-            </div>
+          <div className="m-8 flex items-center">
+            <button className="me-1 border-2 p-2 border-orange-500 bg-white rounded-lg">
+              <i className="fa-solid fa-bolt m-1 text-orange-500"></i>
+              CONNECT TO CRM
+            </button>
+            <button className="me-2 border-2 p-2 text-white bg-blue-700 rounded-lg">
+              <i className="fa-solid fa-circle-plus m-1"></i>
+              SAVE
+            </button>
+            <button className="mx-2">
+              <i className="fa-solid fa-ellipsis-vertical text-3xl"></i>
+            </button>
           </div>
         </div>
+      </div>
 
-        <div className="flex justify-start items-center mt-8">
+      <div className="flex justify-start items-center mt-8">
+        {companyOverviewLoading ? (
+          <div>Loading...</div>
+        ) : companyOverviewError ? (
+          <div>Error loading data. Please try again later.</div>
+        ) : (
           <div className="w-1/3 mx-10 pe-4 border-e-2">
             {companyOverviewData ? (
               <div>
@@ -112,7 +102,9 @@ const Company = () => {
 
                 <div className="flex justify-start items-center my-3 text-sm text-neutral-600">
                   <i className="fa-solid fa-flag pe-1"></i>
-                  <p className="mx-1">{companyOverviewData?.Sector ?? "No Sector avialable"}</p>
+                  <p className="mx-1">
+                    {companyOverviewData?.Sector ?? "No Sector avialable"}
+                  </p>
                 </div>
 
                 <div className="flex justify-start items-center my-3 text-sm text-neutral-600">
@@ -127,19 +119,27 @@ const Company = () => {
 
                 <div className="flex justify-start items-center my-3 text-sm text-neutral-600">
                   <i className="fa-solid fa-signal pe-1"></i>
-                  <p className="mx-1" >{companyOverviewData?.CIK ?? "No Data avialable"}</p>
+                  <p className="mx-1">
+                    {companyOverviewData?.CIK ?? "No Data avialable"}
+                  </p>
                 </div>
-
               </div>
             ) : companyOverviewError ? (
               <div>Error loading data. Please try again later.</div>
             ) : null}
           </div>
+        )}
 
-          <div className="w-2/3 me-8">
+        <div className="w-2/3 me-8">
+          {incomeStatementLoading ? (
+            <div>Loading...</div>
+          ) : incomeStatementError ? (
+            <div>Error loading data. Please try again later.</div>
+          ) : (
             <BarChart xAxisData={xAxisData} yAxisData={yAxisData} />
-          </div>
+          )}
         </div>
+      </div>
     </>
   );
 };
